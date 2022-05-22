ad66077ba1f5fac34a7b1e7b6b5ac12a38d949bc
KushalKhadka7 <crkushal7@gmail.com>
2022-05-22 21:33:11 +0545

    Update docker file

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -1,8 +1,7 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use('/',(req, res, next)=> res.json({message:"Hello wrold"}))
+app.use('/', (req, res, next) => res.json({ message: 'Hello wrold' }));
 
-
-app.listen(3000,'localhost',()=> console.info(`Applicatin running on port: 3000`))
+app.listen(5000, () => console.info(`Application running on port: 5000`));
