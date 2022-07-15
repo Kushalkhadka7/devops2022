0fd45bf9a4acb2e9d7a1499e70b3d2248483181d
crkushal <crkushal@pop-os.localdomain>
2022-07-15 12:46:22 +0545

    Update

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res, next) => res.json({ message: 'Hello wrold' }));
+app.use('/', (req, res, next) => res.json({ message: 'Hello there' }));
 
 app.listen(5000, () => console.info(`Application running on port: 5000`));
